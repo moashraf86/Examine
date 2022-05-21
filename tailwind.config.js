3fb9223f49cba3bd3e78ad6098c7c11d4e14080f
Mohamed Ashraf <botoashraf123@gmail.com>
2022-05-21 04:26:55 +0200

    signup page

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+      'purple'       : '#5E3B76',
+      'purple-400'   : '#826595',
+      'purple-200'   : '#C9BDD2',
+      'seconary'     : '#25A1AF',
+      'info'         : '#E2D9F3',
+      'tertiary'     : '#6C757D',
+      'danger'       : '#DC3545',
+      'light'        : '#F8F9FA',
+      'light-200'    : '#F6F6F6',
+      'border-light' : '#EDF2F7',
+      'cautions'     : '#FDEFEF',
+      'premium'      : '#FFD747',
+      'dark'         : '#212529',
+      'white'        : '#FFFFFF',
+      'transparent'  : 'transparent',
+      'gray-400'     : '#CED4DA'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      },
+      boxShadow: {
+        'innerShadow' : 'inset 0 0 18px -4px rgba(0,0,0,.15)',
+      },
+    },
+    container: {
+      center : true
+    }
+
+    
+  },
+  plugins: [],
+}
